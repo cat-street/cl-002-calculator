d1d40381f9e6e7d88fb7ddc087f80935d18679e3
Andrey Kudryavtsev <lomastering@gmail.com>
2020-10-03 16:32:45 +0300

    Updates

---
+++ precache-manifest.a2fd16ec8acc81cd58bdadc92a23811e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7fbdb00862a58a147172dea2cc08f9c",
+    "url": "/cl-002-calculator/index.html"
+  },
+  {
+    "revision": "83931442baa167240678",
+    "url": "/cl-002-calculator/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "0ffd5b2697ba01aa60b2",
+    "url": "/cl-002-calculator/static/css/main.98fd2e8d.chunk.css"
+  },
+  {
+    "revision": "83931442baa167240678",
+    "url": "/cl-002-calculator/static/js/2.a446e422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cl-002-calculator/static/js/2.a446e422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ffd5b2697ba01aa60b2",
+    "url": "/cl-002-calculator/static/js/main.8caafe76.chunk.js"
+  },
+  {
+    "revision": "a39415d1f451480b394a",
+    "url": "/cl-002-calculator/static/js/runtime-main.4b60e9f9.js"
+  }
+]);
